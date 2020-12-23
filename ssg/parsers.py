e655e45a856014821f74a2fa69fd00c7f1504a94
Lucy Hodgson <32247654+lmhodgson@users.noreply.github.com>
2020-12-23 15:48:47 +0000

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,5 +1,6 @@
 from typing import List
 from pathlib import Path
+import shutil
 
 
 class Parser:
@@ -10,4 +11,16 @@ class Parser:
 
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
+
+    def read(self, path):
+        with open(path, "r") as file:
+            return file.read()
+
+    def write(self, path, dest, content, ext=".html"):
+        full_path = self.dest / path.with_suffix(ext).name
+        with open(full_path) as file:
+            file.write(content)
+
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
 
