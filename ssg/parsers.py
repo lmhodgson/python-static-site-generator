a08f896b8559e9373fb37dce33e9d4a1cfa70261
Lucy Hodgson <32247654+lmhodgson@users.noreply.github.com>
2020-12-23 15:41:48 +0000

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,5 +3,11 @@ from pathlib import Path
 
 
 class Parser:
-    pass
+    extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
 
