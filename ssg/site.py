cbf72435c8cb1de6641d2daadff84daaaff00154
Lucy Hodgson <32247654+lmhodgson@users.noreply.github.com>
2020-12-23 08:43:41 +0000

    Progress

--- ssg/site.py
+++ ssg/site.py
@@ -1,10 +1,10 @@
-import pathlib
+from pathlib import Path
 
 
 class Site:
     def __init__(self, source, dest):
-        self.source = pathlib.Path(source)
-        self.dest = pathlib.Path(dest)
+        self.source = Path(source)
+        self.dest = Path(dest)
 
     def create_dir(self, path):
         directory = self.dest + "/" + path.relative_to(self.source)
