56bfa487479d812ac082fc2a340efb0301aa5459
Lucy Hodgson <32247654+lmhodgson@users.noreply.github.com>
2020-12-31 17:09:04 +0000

    Update content.py

---
+++ ssg/content.py
@@ -0,0 +1,9 @@
+import re
+from yaml import load
+from yaml import FullLoader
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+    __delimiter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
