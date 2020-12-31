60088468d641a40d0519e51ed3959ece8caedb98
Lucy Hodgson <32247654+lmhodgson@users.noreply.github.com>
2020-12-31 16:48:20 +0000

    Updates

--- ssg/site.py
+++ ssg/site.py
@@ -16,3 +16,15 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("Not Implemented")
